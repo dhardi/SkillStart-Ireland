efa9a76f82b4b8f8a5ea7c69358b1545ae02624e
dhardi <50672400+dhardi@users.noreply.github.com>
2026-07-23 17:20:44 +0100

    add new style details templait

--- courses/urls.py
+++ courses/urls.py
@@ -3,7 +3,18 @@ from django.urls import path
 from . import views
 
 
+app_name = "courses"
+
+
 urlpatterns = [
-    path("", views.course_list, name="course_list"),
-    path("<slug:slug>/", views.course_detail, name="course_detail"),
+    path(
+        "",
+        views.course_list,
+        name="course_list",
+    ),
+    path(
+        "<slug:slug>/",
+        views.course_detail,
+        name="course_detail",
+    ),
 ]
